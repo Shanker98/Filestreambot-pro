5c27be7a4bad795e0b6b3191914afa4f6df174ca
Shanker98 <hemshanker9817@gmail.com>
2024-07-10 00:53:14 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,25 +11,25 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '19323618'))
     API_HASH = str(getenv('API_HASH', '94e4717c916c78b5c76af0c1bf274340'))
-    BOT_TOKEN = str(getenv('7460820812:AAHvGoxQv2wCkM2_c3pWIdImh5anQ_TaRZQ'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7460820812:AAHvGoxQv2wCkM2_c3pWIdImh5anQ_TaRZQ'))
     name = str(getenv('name', 'filetolinkbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('-1002180351100', ''))
-    PORT = int(getenv('PORT', '8080'))
-    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '159.28.136.238'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002212201178'))
+    PORT = int(getenv('PORT', '9090'))
+    BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '87.0.4280.101'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "5985614927").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('Pornobox7_bot', ''))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Ssss_1_1_s'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
     
     else:
         ON_HEROKU = False
-    FQDN = str(getenv('FQDN', 'BIND_ADRESS:PORT', '159.28.136.238')) if not ON_HEROKU or getenv('FQDN', '159.28.136.238') else APP_NAME+'.herokuapp.com'
+    FQDN = str(getenv('FQDN', '87.0.4280.101:9090')) if not ON_HEROKU or getenv('FQDN', '87.0.4280.101:9090') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
